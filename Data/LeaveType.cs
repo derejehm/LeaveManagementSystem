2d2e5f6fb11b0e934edec338c2431e3306dd755f
dereje <hailemariam013@gmail.com>
2025-04-15 18:06:03 +0300

    Completed Leave Type Management Module

---
+++ Data/LeaveType.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace LeaveManagementSystem.Web.Data
+{
+    public class LeaveType
+    {
+        public int Id { get; set; }
+        [Column(TypeName = "nvarchar(150)")]
+        public String Name { get; set; }
+        public int NumberOfDays { get; set; }
+    }
+}
